38fb6c5d1986041de388e1ef254ed0fd2155616a
Anton Harniakou <anton.harniakou@gmail.com>
2020-02-18 14:12:55 +0300

    Use winston logger

--- src/server.js
+++ src/server.js
@@ -619,7 +619,7 @@ appV1.post(
               subject: "Регистрация в Job Sova",
               bodyHtml: body
             });
-            console.log(body)
+            console.log(body);
           },
           nanoid,
           async token => `localhost:3000/activate?token=${token}`
@@ -651,13 +651,13 @@ appV1.post(
         require("./use-cases/user-begins-email-change")(
           UserService,
           async (email, url) => {
-            const body = `Для завершения регистрации пройдите по ссылке <a href="${url}">${url}</a>`
+            const body = `Для завершения регистрации пройдите по ссылке <a href="${url}">${url}</a>`;
             await sendMail({
               to: [email],
               subject: "Регистрация в Job Sova",
               bodyHtml: body
-            })
-            console.log(body)
+            });
+            console.log(body);
           },
           nanoid,
           async token => `localhost:3000/activate?token=${token}`
@@ -873,9 +873,9 @@ appV1.post(
 appV1.get(
   "/change-email/:token",
   toCallback(
-      require("./controllers/user-finishes-email-change")(
-        require("./use-cases/user-finishes-email-change")(UserService)
-      )
+    require("./controllers/user-finishes-email-change")(
+      require("./use-cases/user-finishes-email-change")(UserService)
+    )
   )
 );
 
@@ -1083,6 +1083,23 @@ appV1.put(
   )
 );
 
+const expressWinston = require("express-winston");
+const winston = require("winston");
+const logger = expressWinston.logger({
+  transports: [new winston.transports.Console()],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json()
+  ),
+  meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+  msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+  expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+  colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+  ignoreRoute: function(req, res) {
+    return false;
+  } // optional: allows to skip some log messages based on request and/or response
+});
+app.use(logger);
 app.use("/api/v1", appV1);
 
 module.exports = { httpServer: app, wsServer: io };
